5c1c19951dcd3c80d1792c1d01df7c6c824889b3
Madhav Poudel <king.ov.universe@gmail.com>
2018-04-28 23:08:51 +0545

    Added Logo

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { View, Image } from 'react-native';
 import { Toolbar } from 'react-native-material-ui';
 
+import { LPText } from '../Text';
+import logo from '../../assets/images/Khata-light.png';
+
 const AppBar = () => (
-  // TODO: Logo
   <Toolbar
-    centerElement="Khata"
+    centerElement={
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image style={{ height: 35, width: 35 }} source={logo} />
+        <LPText
+          style={{ fontSize: 28, fontWeight: '500', fontFamily: 'cabin-regular', marginLeft: 12 }}
+        >
+          Khata
+        </LPText>
+      </View>
+    }
     style={{
       container: {
         backgroundColor: '#2c3e50',
